2d0b6923f8a8441e4636be5c8ed4734dc6798937
Ashwin Bhargava <contact@ashwincodes.com>
2023-10-25 18:35:12 +0530

    feat: add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,20 @@
+import { AiOutlineInstagram as InstagramIcon } from "react-icons/ai"
+import { FaXTwitter as TwitterIcon } from "react-icons/fa6"
+import { CiFacebook as FacebookIcon } from "react-icons/ci"
+
 export default function Footer() {
-	return <div>Footer</div>
+	const currentYear = new Date().getFullYear()
+	return (
+		<footer className="bg-black text-white py-4 px-2 flex flex-col items-center gap-4">
+			<h1 className="md:text-xl xl:text-2xl">Follow Us On</h1>
+			<div className="flex gap-2 text-3xl">
+				<InstagramIcon className="cursor-pointer" />
+				<TwitterIcon className="cursor-pointer" />
+				<FacebookIcon className="cursor-pointer" />
+			</div>
+			<h2>
+				{`© ${currentYear}`} <span className="text-pizza-orange">PizzaVerse</span> All Rights Reserved
+			</h2>
+		</footer>
+	)
 }
