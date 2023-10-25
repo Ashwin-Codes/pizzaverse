6632e694d06d9398e47d7bc39b192be5754ac77c
Ashwin Bhargava <contact@ashwincodes.com>
2023-10-25 17:20:52 +0530

    feat: add react-router and setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Navigate } from "react-router-dom"
+
 export default function App() {
-	return <div>App</div>
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path="/">
+				<Route index element={<h1>Pizza</h1>}></Route>
+				<Route path="cart" element={<h1>cart</h1>}></Route>
+				<Route path="*" element={<Navigate to="/" />}></Route>
+			</Route>
+		)
+	)
+	return <RouterProvider router={router} />
 }
