f945c1341b08a9a19bd374b25a05023f2b712dc3
Ashwin Bhargava <contact@ashwincodes.com>
2023-10-25 17:33:20 +0530

    feat: add tailwindCSS and default layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Navigate } from "react-router-dom"
+import Layout from "./components/Layout"
 
 export default function App() {
 	const router = createBrowserRouter(
 		createRoutesFromElements(
-			<Route path="/">
+			<Route path="/" element={<Layout />}>
 				<Route index element={<h1>Pizza</h1>}></Route>
 				<Route path="cart" element={<h1>cart</h1>}></Route>
 				<Route path="*" element={<Navigate to="/" />}></Route>
