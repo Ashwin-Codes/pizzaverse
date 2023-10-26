ae061ed84600b50d3c2be7d5e20b653d149ea335
Ashwin Bhargava <contact@ashwincodes.com>
2023-10-26 15:48:02 +0530

    feat: add FilterButton.jsx component and logic to filter pizza based on veg and non-veg

---
+++ src/features/pizza/FilterButtons.jsx
@@ -0,0 +1,52 @@
+import { useSearchParams } from "react-router-dom"
+
+export default function SortButtons() {
+	const [searchParams, setSearchParams] = useSearchParams()
+	const filter = searchParams.get("filter")
+	const sort = searchParams.get("sort")
+
+	function toggleVegFilterParams() {
+		const queries = {}
+		if (filter !== "veg") {
+			queries.filter = "veg"
+		}
+
+		// Preserve Sort
+		if (sort) {
+			queries.sort = sort
+		}
+		setSearchParams(queries)
+	}
+
+	function toggleNonvegFilterParams() {
+		const queries = {}
+		if (filter !== "nonveg") {
+			queries.filter = "nonveg"
+		}
+
+		// Preserve Sort
+		if (sort) {
+			queries.sort = sort
+		}
+		setSearchParams(queries)
+	}
+
+	return (
+		<div className="w-11/12 mx-auto 2xl:w-[85%] flex gap-2 py-4">
+			<button
+				className={`px-2 rounded-md  ${
+					filter === "veg" ? "bg-pizza-orange text-white" : "bg-gray-100 text-olive-black"
+				}`}
+				onClick={toggleVegFilterParams}>
+				Veg
+			</button>
+			<button
+				className={`px-2 rounded-md  ${
+					filter === "nonveg" ? "bg-pizza-orange text-white" : "bg-gray-100 text-olive-black"
+				}`}
+				onClick={toggleNonvegFilterParams}>
+				Non-veg
+			</button>
+		</div>
+	)
+}
--- src/features/pizza/PizzaListing.jsx
+++ src/features/pizza/PizzaListing.jsx
@@ -4,18 +4,38 @@ import { getAllPizza, getPizzaStatus } from "./pizzaSlice"
 import { REQUEST_STATUS } from "../../config/requestStatus"
 import { fetchAllPizza } from "./pizzaSlice"
 import PizzaCard from "./PizzaCard"
+import SortButtons from "./FilterButtons"
+import { useSearchParams } from "react-router-dom"
 
 export default function PizzaListing() {
 	const initiated = useRef(false) // Dispatched state
-	const pizzaStatus = useSelector(getPizzaStatus)
+	const pizzaStatus = useSelector(getPizzaStatus) // Network request status
 	const dispatch = useDispatch()
+	const [searchParams] = useSearchParams()
 
 	const allPizza = useSelector(getAllPizza)
 	const [pizza, setPizza] = useState(allPizza ? allPizza : [])
 
 	useEffect(() => {
-		setPizza(allPizza ? allPizza : [])
-	}, [allPizza])
+		const filter = searchParams.get("filter")
+		let filteredAndSortedPizza = allPizza ? allPizza : []
+
+		// Filter
+		if (filter) {
+			if (filter === "veg") {
+				filteredAndSortedPizza = filteredAndSortedPizza.filter((singlePizza) => {
+					return singlePizza.isVeg
+				})
+			}
+			if (filter === "nonveg") {
+				filteredAndSortedPizza = filteredAndSortedPizza.filter((singlePizza) => {
+					return !singlePizza.isVeg
+				})
+			}
+		}
+
+		setPizza(filteredAndSortedPizza)
+	}, [allPizza, searchParams])
 
 	useEffect(() => {
 		if (pizzaStatus === REQUEST_STATUS.IDLE && !initiated.current) {
@@ -25,10 +45,13 @@ export default function PizzaListing() {
 	}, [pizzaStatus, dispatch])
 
 	return (
-		<div className="flex flex-wrap gap-4 font-inter mb-24 xl:w-[90%] xl:mx-auto xl:gap-6">
-			{pizza.map((singlePizza) => {
-				return <PizzaCard key={singlePizza.id} pizza={singlePizza} />
-			})}
-		</div>
+		<>
+			<SortButtons />
+			<div className="flex flex-wrap gap-4 font-inter mb-24 xl:w-[90%] xl:mx-auto xl:gap-6">
+				{pizza.map((singlePizza) => {
+					return <PizzaCard key={singlePizza.id} pizza={singlePizza} />
+				})}
+			</div>
+		</>
 	)
 }
